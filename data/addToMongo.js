b362872304c2e282726f1fbfc38096cdce455165
Stewart Douglas <stewart.douglas13@imperial.ac.uk>
2015-01-05 17:54:29 +0000

    Modified addToMongo.js

--- data/addToMongo.js
+++ data/addToMongo.js
@@ -5,20 +5,22 @@ MongoClient.connect("mongodb://localhost:27017/kyc",function(err,db){
 		console.log('Error connecting to Mongo');
 	} else {
 		console.log('Successfully connected to Mongo');
+		var count = 0;
 
-		require("./US_CongressMembers/USRepresentatives.json").forEach(function(element){
+		require("./US_Sanctions/UStargetsUpdated.json").forEach(function(element){
+			count +=1 
 			db.collection('targets').insert(element,{w:0});
 		});
 		
-		require("./US_CongressMembers/USSenators.json").forEach(function(element){
-			db.collection('targets').insert(element,{w:0});	
-		});		
+		// require("./US_CongressMembers/USSenators.json").forEach(function(element){
+		// 	db.collection('targets').insert(element,{w:0});	
+		// });		
 
-		require("./World_Bank_Sanctions/WorldBankSanctions.json").forEach(function(element){
-			db.collection('targets').insert(element,{w:0});
-		});
+		// require("./World_Bank_Sanctions/WorldBankSanctions.json").forEach(function(element){
+		// 	db.collection('targets').insert(element,{w:0});
+		// });
 
-		console.log("Elements added to MongoDB");	
+		console.log(count + " elements added to MongoDB");	
 
 	}
 });
