3f930c9f44f178c85b44ddff3cfe03a5441f23e4
Stewart Douglas <stewart.douglas13@imperial.ac.uk>
2015-01-06 09:59:22 +0000

    Changed UStargets to under score

--- data/US_Sanctions/parseJSON.js
+++ data/US_Sanctions/parseJSON.js
@@ -12,7 +12,7 @@ targets.forEach(function(target){
 	entity.data_source = "US Sanctions List";
 	entity.uri = "http://www.treasury.gov/resource-center/sanctions/SDN-List/Pages/consolidated.aspx";
 
-	entity.fullName = target.firstName + " " + target.lastName;
+	entity.full_name = target.firstName + " " + target.lastName;
 	var tempFirstName = target.firstName.split(" ");
 	entity.first_name = tempFirstName[0];
 	entity.last_name = target.lastName;
@@ -30,26 +30,35 @@ targets.forEach(function(target){
 	if(target.akaList){
 		entity.aka_list = new Object();
 		if(target.akaList.aka.length === 2){
-			entity.aka_list.aka1 = target.akaList.aka[0];
-			entity.aka_list.aka2 = target.akaList.aka[1];
+			entity.aka_list.aka1 = generateAKA(target.akaList.aka[0]);
+			entity.aka_list.aka2 = generateAKA(target.akaList.aka[1]);
 		} else if(target.akaList.aka.length === 3){
-			entity.aka_list.aka1 = target.akaList.aka[0];
-			entity.aka_list.aka2 = target.akaList.aka[1];
-			entity.aka_list.aka3 = target.akaList.aka[2];
+			entity.aka_list.aka1 = generateAKA(target.akaList.aka[0]);
+			entity.aka_list.aka2 = generateAKA(target.akaList.aka[1]);
+			entity.aka_list.aka3 = generateAKA(target.akaList.aka[2]);
 		} else {
-			entity.aka_list.aka1 = target.akaList.aka;
+			entity.aka_list.aka1 = generateAKA(target.akaList.aka);
 		}
-
 	}
-
 	results.push(entity);
 });
 
 // save file
 saveToJSON(results, './UStargetsUpdated.json')
 
-function saveToJSON(results, fileName){
+// used to convert keys from camelCase to under_score
+function generateAKA(jsonObj){
+	var result = new Object();
+	result.uid = jsonObj.uid;
+	result['type'] = jsonObj['type'];
+	result.category = jsonObj.category;
+	result.first_name = jsonObj.firstName;
+	result.last_name = jsonObj.lastName;
+	return result;
+}
 
+// used to save files
+function saveToJSON(results, fileName){
   fs.writeFile(fileName, JSON.stringify(results, null, 4), function(err) {
     if(err) {
       console.log(err);
